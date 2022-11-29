a44f295d792a49fbb37016a54ab43ea1a389d745
Reni <reinich@outlook.com>
2022-11-29 17:58:44 +0300

    Add Task1

---
+++ 29.11.2022/Task1 Min and Max/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите первое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+if (n>m || n==m)
+{
+    Console.WriteLine($"Наибольшее значение: {n}");
+    Console.WriteLine($"Наименьшее значение: {m}");
+}
+else 
+{
+    Console.WriteLine($"Наибольшее значение: {m}");
+    Console.WriteLine($"Наименьшее значение: {n}");
+}
