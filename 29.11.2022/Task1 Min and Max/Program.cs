35259a21f16343eb097e2a29a2639732916214a4
Reni <reinich@outlook.com>
2022-11-29 18:19:58 +0300

    Add Task2

--- 29.11.2022/Task1 Min and Max/Program.cs
+++ 29.11.2022/Task1 Min and Max/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.Write("Введите первое число: ");
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
 int n = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int m = Convert.ToInt32(Console.ReadLine());
---
+++ 29.11.2022/Task2 Max/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int i = Convert.ToInt32(Console.ReadLine());
+
+int max=0;
+
+if (n>m)
+{    if (n>i)
+        max=n;
+}
+else if (n==m)
+    max=n;
+
+if (m>i)
+{    if (m>n)
+        max=m;
+}
+else if (m==i)
+    max=m;
+
+if (i>n)
+{    if (i>m)
+        max=i;
+}
+else if (i==n)
+    max=i;
+
+Console.WriteLine("Наибольшее значение: " + max);
+
+
