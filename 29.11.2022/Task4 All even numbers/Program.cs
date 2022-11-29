d3ca12a0afa7adb8e098c682f19885c9b8d96c63
Reni <reinich@outlook.com>
2022-11-29 20:23:19 +0300

    Add fix2

--- 29.11.2022/Task4 All even numbers/Program.cs
+++ 29.11.2022/Task4 All even numbers/Program.cs
@@ -3,7 +3,8 @@ Console.Write("Введите число: ");
 int n = Convert.ToInt32(Console.ReadLine());
 
 for (int i=1; i<=n; i++)
-{   if (i%2==0)
-        Console.Write(i+" ");
+{   
+        if (i%2==0)
+                Console.Write(i+" ");
 }
 
