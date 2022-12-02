830fa5a65fcc74efa76e7a4ffde2b932f98119b0
Reni <reinich@outlook.com>
2022-12-02 23:36:59 +0300

    Add fix

--- 02.12.2022/Task 3 Number of week/Program.cs
+++ 02.12.2022/Task 3 Number of week/Program.cs
@@ -8,17 +8,5 @@ while (n < 1 || n > 7)
     Console.Write("Вы ошиблись. \n Введите номер дня недели: ");
     n = Convert.ToInt32(Console.ReadLine());
 }
-if (n == 1)
-    Console.Write("No");
-else if (n == 2)
-    Console.Write("No");
-else if (n == 3)
-    Console.Write("No");
-else if (n == 4)
-    Console.Write("No");
-else if (n == 5)
-    Console.Write("No");
-else if (n == 6)
-    Console.Write("Yes");
-else 
-    Console.Write("Yes");
+if (n==6 || n == 7) Console.WriteLine("Yes");
+else Console.Write("No");
