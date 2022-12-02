279af27cba67c9b06888a5a4024e2c539812d5fb
Reni <reinich@outlook.com>
2022-12-02 14:26:00 +0300

    Add Task 2

--- 02.12.2022/Task 2/Program.cs
+++ 02.12.2022/Task 2/Program.cs
@@ -3,5 +3,15 @@
 Console.Write("Введите число: ");
 int n = Convert.ToInt32(Console.ReadLine());
 
+while (n <= 99)
+{
+    Console.Write("Вы ошиблись. \n Введите новое число: ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
 
+while (n>999)   
+{
+    n = n/10;
+}  
 
+if (n>=100 && n<=999) Console.WriteLine($"Третье число: {n%10}");
