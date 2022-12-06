4cc556e1b592afbb165f532d83c9d7266ddd15d2
Reni <reinich@outlook.com>
2022-12-06 16:31:55 +0300

    Add fix

--- 06.12.2022/Task 1 Palindrom/Program.cs
+++ 06.12.2022/Task 1 Palindrom/Program.cs
@@ -1,17 +1,15 @@
 ﻿Console.Clear();
 int n = 5;
-int[] a = new int[n];
 Console.WriteLine("Вводите числа: ");
-for (int i = 0; i < n; i++)
-{
-    a[i] = Convert.ToInt32(Console.ReadLine());
-}
+
+int numbers = Convert.ToInt32(Console.ReadLine());
+string b = numbers.ToString();
 
 int count = 0;
 
 for (int i = 0; i < (n/2); i++)
 {
-    if (a[i]==a[n-i-1]) count++;
+    if (b[i]==b[n-i-1]) count++;
     else 
     {
         Console.WriteLine("Это число не палиндром");
