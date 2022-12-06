368224049886a08ca34a922b8b9015059e8feae2
Reni <reinich@outlook.com>
2022-12-06 14:21:06 +0300

    Add 3 tasks 06.12.2022

---
+++ 06.12.2022/Task 1 Palindrom/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+int n = 5;
+int[] a = new int[n];
+Console.WriteLine("Вводите числа: ");
+for (int i = 0; i < n; i++)
+{
+    a[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+int count = 0;
+
+for (int i = 0; i < (n/2); i++)
+{
+    if (a[i]==a[n-i-1]) count++;
+    else 
+    {
+        Console.WriteLine("Это число не палиндром");
+        break;
+    }
+    if (count == 2) Console.WriteLine("Это число палиндром");
+}
---
+++ 06.12.2022/Task 2 Distance between coord 3D/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите первые координаты: ");
+double x1 = Convert.ToInt32(Console.ReadLine());
+double y1 = Convert.ToInt32(Console.ReadLine());
+double z1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите вторые координаты: ");
+double x2 = Convert.ToInt32(Console.ReadLine());
+double y2 = Convert.ToInt32(Console.ReadLine());
+double z2 = Convert.ToInt32(Console.ReadLine());
+
+double l = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2) + Math.Pow(z1 - z2, 2));
+Console.WriteLine($"Расстояние = {Math.Round(l, 2)}");
