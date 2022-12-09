9e1d33d7ab2a80538ee9d0e60fd005d5b34d1410
Reni <reinich@outlook.com>
2022-12-09 16:58:11 +0300

    Add some fix2

--- 09.12.2022/Task 5 Prime Even number/Program.cs
+++ 09.12.2022/Task 5 Prime Even number/Program.cs
@@ -11,11 +11,15 @@ bool Simple(int num)
     }
     return true;
 }
+
+
 while (num < 4 || num % 2 != 0)
 {
     Console.Write($"Разве это похоже на четное число или на число, которое больше 3?\nВведите ЧЕТНОЕ число, которое БОЛЬШЕ 3-х");
     num = Convert.ToInt32(Console.ReadLine());
 }
+
+
 for (int i = 2; i < num; i++)
 {
     if (Simple(i) == true && Simple(num - i) == true)
