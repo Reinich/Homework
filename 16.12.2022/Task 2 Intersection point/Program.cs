908df431fd4d529495081ee3a3bc147d50cd29d5
Reni <reinich@outlook.com>
2022-12-18 16:45:36 +0300

    Add changes

--- 16.12.2022/Task 2 Intersection point/Program.cs
+++ 16.12.2022/Task 2 Intersection point/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.Clear();
 Console.Write("Введите k1, k2, b1, b2: ");
-int[] x = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+double[] x = Console.ReadLine().Split(" ").Select(x => double.Parse(x)).ToArray();
 
-Console.WriteLine($"Точка пересечение: {(double)(x[2] - x[3]) / Math.Abs(x[0] - x[1])}; {(double)(x[1] * (double)(x[2] - x[3]) / Math.Abs(x[0] - x[1])) + x[3]}");
+Console.WriteLine($"Точка пересечение: {(x[2] - x[3]) / Math.Abs(x[0] - x[1])}; {(x[1] * (x[2] - x[3]) / Math.Abs(x[0] - x[1])) + x[3]}");
 
 
