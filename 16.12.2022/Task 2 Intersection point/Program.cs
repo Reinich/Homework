4448a5a3fb3dd956c9092a79ab143b6b9f2e917c
Reni <reinich@outlook.com>
2022-12-18 17:09:11 +0300

    Add changes again

--- 16.12.2022/Task 2 Intersection point/Program.cs
+++ 16.12.2022/Task 2 Intersection point/Program.cs
@@ -2,6 +2,9 @@
 Console.Write("Введите k1, k2, b1, b2: ");
 double[] x = Console.ReadLine().Split(" ").Select(x => double.Parse(x)).ToArray();
 
-Console.WriteLine($"Точка пересечение: {(x[2] - x[3]) / Math.Abs(x[0] - x[1])}; {(x[1] * (x[2] - x[3]) / Math.Abs(x[0] - x[1])) + x[3]}");
+if (x[0] != x[1]) Console.WriteLine($"Точка пересечения: {(x[2] - x[3]) / Math.Abs(x[0] - x[1])}; {(x[1] * (x[2] - x[3]) / Math.Abs(x[0] - x[1])) + x[3]}");
+else Console.WriteLine("Прямые параллельны и точек пересечения нет");
+
+
 
 
