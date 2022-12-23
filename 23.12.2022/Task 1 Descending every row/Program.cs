c117b7a1267ab55888bb4a128c7da7d019ae7557
Reni <reinich@outlook.com>
2022-12-23 19:50:51 +0300

    Add first task

--- 23.12.2022/Task 1 Descending every row/Program.cs
+++ 23.12.2022/Task 1 Descending every row/Program.cs
@@ -13,29 +13,41 @@ void InputMatrix(int[,] matrix)
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
-            matrix[i, j] = new Random().Next(-10, 11);
+            matrix[i, j] = new Random().Next(1, 11);
     }
     OutputMatrix(matrix);
 }
-
-void Descending(int[,] matrix)
-{
-    int max = 0;
+ 
+ 
+void SortingArray(int[,] matrix) 
+{ 
+    int tmp = 0;  
+    int[] array = new int[matrix.GetLength(1)];
+    int n = array.GetLength(0);
+    
+    
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            
-        }
+        for (int j = 0; j < matrix.GetLength(1); j++)              // В созданный одномерный массив запихиваем все значения строчки
+            array[j] = matrix[i, j]; 
 
+        for (int k = 0; k < n; k++)                                // Сортируем одномерный массив
+        { 
+            for (int l = n - 1; l > k; l--) 
+            { 
+                if (array[k] < array[l]) 
+                { 
+                    tmp = array[k]; 
+                    array[k] = array[l]; 
+                    array[l] = tmp; 
+                } 
+            } 
+        } 
+        
+        for (int j = 0; j < matrix.GetLength(1); j++)             // Заменяем старые значения новым одномерным массивом
+            matrix[i,j] = array[j];   
     }
 }
-
-
-
-
-
-
 
 
 
@@ -43,6 +55,9 @@ Console.Clear();
 Console.Write("Введите размеры матрицы: ");
 int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
 int[,] matrix = new int[size[0], size[1]];
+Console.WriteLine("Начальный массив: ");
 InputMatrix(matrix);
-
-Descending(matrix);
+SortingArray(matrix);
+Console.WriteLine();
+Console.WriteLine("Конечный массив: ");
+OutputMatrix(matrix);
