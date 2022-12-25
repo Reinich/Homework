0206edbb6e21a7f1045971cbecec7a22c2065f04
Reni <reinich@outlook.com>
2022-12-25 21:21:09 +0300

    Add 24.12.2022

---
+++ 24.12.2022/Task 1 Sum of num bw n and m/Program.cs
@@ -0,0 +1,17 @@
+﻿// Найти сумму всех чисел от N до M
+
+int rec(int m, int n)
+{
+    int sum = n;
+    if (n == m)
+        return sum;
+    return sum += rec(m, n - 1) ;
+}
+
+
+Console.Clear();
+Console.Write("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(rec(m, n));
