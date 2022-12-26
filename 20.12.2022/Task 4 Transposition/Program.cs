3c80a7ef0b9c56167e1ae6509bc459cae2827e28
Reni <reinich@outlook.com>
2022-12-26 22:00:39 +0300

    Change Transposition

--- 20.12.2022/Task 4 Transposition/Program.cs
+++ 20.12.2022/Task 4 Transposition/Program.cs
@@ -20,18 +20,9 @@ void Transposition(int[,] matrix)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            if (matrix.GetLength(0) % 2 == 0)
-            {
-                temporary = matrix[i, j];
-                matrix[i, j] = matrix[matrix.GetLength(1) - i - 1, j];
-                matrix[matrix.GetLength(1) - i - 1, j] = temporary;
-            }
-            else
-            {
-                temporary = matrix[i, j];
-                matrix[i, j] = matrix[matrix.GetLength(1) - i, j];
-                matrix[matrix.GetLength(1) - i, j] = temporary;                
-            }
+            temporary = matrix[i, j];
+            matrix[i, j] = matrix[matrix.GetLength(0) - i - 1, j];
+            matrix[matrix.GetLength(0) - i - 1, j] = temporary;
         }
     }
 }
@@ -52,8 +43,8 @@ int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
 int[,] matrix = new int[size[0], size[1]];
 
 InputMatrix(matrix);
-
 Console.WriteLine();
+
 Transposition(matrix);
 
 OutputMatrix(matrix);
